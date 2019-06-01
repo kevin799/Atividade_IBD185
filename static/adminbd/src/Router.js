9f90c31b3f252064dd655e95dd1d9a11d88c8233
Lucas Domingos Leão Gomes <lgomes@dannemann.com.br>
2019-05-31 22:46:01 -0300

    Adicionado Rota e pagina Login com front

--- static/adminbd/src/Router.js
+++ static/adminbd/src/Router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './View/Home.vue'
+import Login from './View/Login.vue'
+
 
 Vue.use(Router)
 
@@ -12,6 +14,11 @@ const router = new Router({
       path: '/home',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     },
   
   ]

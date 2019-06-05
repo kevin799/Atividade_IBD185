fa16572e6be98be0742b4feca8d769f0f79663de
joohnnathans <joohnnathans@gmail.com>
2019-06-05 17:37:58 -0300

    ajdo

--- static/adminbd/src/View/Banco.vue
+++ static/adminbd/src/View/Banco.vue
@@ -15,29 +15,29 @@
         <div class="add-bd border border-dark" id="add-bd">
             <h1 class="title" id='add-title'><p>Adicionar</p></h1>
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Name" aria-label="Name" aria-describedby="basic-addon1" v-model="name">
+                <input type="text" class="form-control" placeholder="Name" aria-label="Name" aria-describedby="basic-addon1" v-model="banco.name">
             </div>
       
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Type" aria-label="Type" aria-describedby="basic-addon1" v-model="type">  
-                <input type="text" class="form-control" placeholder="Version" aria-label="Version" aria-describedby="basic-addon1" v-model="version">  
+                <input type="text" class="form-control" placeholder="Type" aria-label="Type" aria-describedby="basic-addon1" v-model="banco.type">  
+                <input type="text" class="form-control" placeholder="Version" aria-label="Version" aria-describedby="basic-addon1" v-model="banco.version">  
             </div>
 
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Port" aria-label="Port" aria-describedby="basic-addon1" v-model="port">
+                <input type="text" class="form-control" placeholder="Port" aria-label="Port" aria-describedby="basic-addon1" v-model="banco.port">
             </div>
       
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Developer" aria-label="Developer" aria-describedby="basic-addon1" v-model="developer">
-                <select class="custom-select mt-2 mr-3" id="inputGroupSelect01">
-                    <option selected>ON</option>
-                    <option value="1">OFF</option>
+                <input type="text" class="form-control" placeholder="Developer" aria-label="Developer" aria-describedby="basic-addon1" v-model="banco.developer">
+                <select class="custom-select mt-2 mr-3" id="inputGroupSelect01" v-model="banco.status">
+                    <option selected>0</option>
+                    <option value="1">1</option>
                 </select>  
                 
             </div>
 
             <div class="d-flex justify-content-center">
-                <button type="button" class="btn btn-success btn-lg" v-on:click="noneElemento">Save</button>
+                <button type="button" class="btn btn-success btn-lg" v-on:click="createBanco()">Save</button>
             </div>
         </div>
 
@@ -67,7 +67,7 @@
 
         <div class="d-flex flex-row">
             <div class="input-group mb-3 col-4 mx-auto">
-                <input type="text" class=" mt-3 form-control" placeholder="Search Server">
+                <input type="text" class=" mt-3 form-control" placeholder="Search Server" v-model="search">
                 <div class="input-group-append">
                     <button class="btn btn-outline-secondary mt-3" type="button" id="search-serve">Search</button>
                 </div>
@@ -86,7 +86,11 @@
                 </tr>
             </thead>
             <tbody class="tbody">
-                <tr id="bds" v-for="teste in testes" :key="teste.name">
+                <tr id="bds" v-for="banco in this.bancoId" :key="banco.name" >
+                    <th class="text-center"> {{banco.name}}</th>
+                </tr>
+
+                <!-- <tr id="bds" v-for="teste in testes" :key="teste.name">
 
                     <th class="text-center"> {{teste.name}}</th>
                     <th class="text-center"> {{teste.port}}</th>
@@ -94,7 +98,7 @@
                     <th class=" text-center"> <button type="button" class="btn btn-success" id="btn-status" v-on:click="teste()">Info</button>
                      <button type="button" class="btn btn-danger" id="btn-status" v-on:click="teste()">Del</button>
                      </th>
-                </tr>
+                </tr> -->
             </tbody>
             <tfoot>
                 <th>Total</th>
@@ -110,19 +114,23 @@
 export default {
     data(){
         return{
-            name:'',
-            port: 0,
-            developer:'',
-            testes:[{'name':'nome1','port':1020, 'developer':'192...'},
-            {'name':'nome2','port':1010, 'developer':'192...'},
-            {'name':'nome3','port':1025, 'developer':'192...'},
-            {'name':'nome4','port':1026, 'developer':'192...'} ]
+            banco:{
+                name:'',
+                type:'',
+                version:'',
+                port:'',
+                developer:'',
+                status:'',
+                id: 0
+            },
+            search: '', 
+            bancoId:''          
         }
 
     },
     mounted(){
-        this.habiServiButton()
-        this.openInfoServ()
+        this.bancoId = this.getbancobyId()
+        console.log(this.bancoId)
     },
     methods:{
         blockElemento(){
@@ -130,14 +138,8 @@ export default {
             document.getElementById('add-bd').style.display = 'block'
 
 
-        },
+        }, 
         noneElemento(){
-            this.testes.push({
-                'name': this.name,
-                'port': this.port,
-                'developer': this.developer
-
-            })
             document.getElementById('add-bd').style.display = 'none'
          
         },
@@ -145,13 +147,48 @@ export default {
         teste(){
             document.getElementById('add-bd').style.display = 'block'
             document.getElementById('add-title').innerHTML = 'Alter BD'
-        }
-    }
+        },
 
+
+        //teste axios
+
+         createBanco () {
+         var params = {
+            'nome': this.name,
+            'tipo': this.type,
+            'versao': this.version,
+            'porta': this.port,
+            'desenvolvedor': this.developer,
+            'status': this.status,
+            'ser_id': 1
+
+        }
+
+        this.$http.post(`/springRest/banco/save`, params)
+        .then(response => {
+            // JSON responses are automatically parsed.
+            this.response = response.data
+            this.banco.id = response.data
+            console.log(response.data)
+            this.showResponse = true
+          })
+
+      
+        },
+        getbancobyId () {
+        this.$http.get(`http://localhost:8082/springRest/banco/findByID?id=1` )//+ this.search)
+          .then(response => {
+            // JSON responses are automatically parsed.
+            this.response = response.data
+            console.log(response.data)
+            this.response = true
+          })
+      }
 
     
    
     
+}
 }
 </script>
 
--- static/adminbd/src/main.js
+++ static/adminbd/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './Router'
+import axios from 'axios'
+
+Vue.prototype.$http = axios
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'

b6b9c3e824289896c9ab79f8e3b7b4b725e8578c
Lucas Domingos Leão Gomes <lgomes@dannemann.com.br>
2019-06-07 17:07:14 -0300

    Adicionando tela de login na raiz

--- static/adminbd/src/Router.js
+++ static/adminbd/src/Router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './View/Home.vue'
 import Banco from './View/Banco.vue'
+import Login from './View/Login.vue'
 Vue.use(Router)
 
 const router = new Router({
@@ -10,6 +11,11 @@ const router = new Router({
   routes: [
     {
       path: '/',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/home',
       name: 'home',
       component: Home
     },
